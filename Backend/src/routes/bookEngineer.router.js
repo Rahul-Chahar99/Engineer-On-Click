326cca8c469eeb52e915f99abd257a7d2543faa3
Rahul Chahar <rahul@example.com>
2026-03-06 12:23:59 +0530

    razorpay working fine

--- Backend/src/routes/bookEngineer.router.js
+++ Backend/src/routes/bookEngineer.router.js
@@ -1,10 +1,11 @@
 import {Router } from 'express'
-import {bookEngineer, deleteEngineerRequest, getAllEngineerRequests} from '../controllers/bookEnginner.controller.js'
+import {bookEngineer, deleteEngineerRequest, getAllEngineerRequests, paymentVerification} from '../controllers/bookEnginner.controller.js'
 import { isAdmin, verifyJWT } from '../middlewares/auth.middleware.js'
 
 const router = Router()
 
 router.route('/book-engineer').post(bookEngineer)
+router.route('/paymentverification').post(paymentVerification)
 router.route('/booking-requests',verifyJWT,isAdmin).get(getAllEngineerRequests)
 router.route('/booking-requests/:id',verifyJWT,isAdmin).delete(deleteEngineerRequest)
 
